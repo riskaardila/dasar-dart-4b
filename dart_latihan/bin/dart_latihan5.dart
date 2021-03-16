a1b0623eed8862ffb697e2091ed3afe5d2838c2a
riskaardila <riskaardila35@gmail.com>
2021-03-16 22:05:19 +0700

    tambahkan

--- dart_latihan/bin/dart_latihan5.dart
+++ dart_latihan/bin/dart_latihan5.dart
@@ -1,18 +1,9 @@
-import 'dart:io';
-
+// Perulangan
 import 'dart:io';
 
 void main(List<String> arguments) {
-  List <int> myList = [7, 1, 2, 3, 4, 5, 6];
-  List<String> list =[];
-   
-  // myList.forEach((element) {
-  //   list.add("angka " + bilangan.toString());
-  // });
-
-  list =  myList.map((number) => "angka " + number.toString()).toList();
-
-  list.forEach((str) {
-    print(str);
-  });
+  int a, b;
+  a = 10;
+  b = a++;
+  print(a.toString() + " - " + b.toString());
 }
