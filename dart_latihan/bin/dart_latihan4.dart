13cbb6d924add71b6dcd8ce0547fb8fb2748e176
riskaardila <riskaardila35@gmail.com>
2021-03-16 20:58:24 +0700

    tambahkan

--- dart_latihan/bin/dart_latihan4.dart
+++ dart_latihan/bin/dart_latihan4.dart
@@ -1,19 +1,25 @@
-// // function
+// function
 
-// import 'dart:io';
+import 'dart:io';
 
-// String say(String from, String message, {String to, String appName}){
-//   return from + " say " + message + ((to ))
-// }
+String say(String from, String message, {String to, String appName: "Whatsapp"}){
+  return from + " say " + message + ((to != null) ? " to " + to : "") + ((appName != null) ? " via " + appName : "");
+}
 
-// main(List<String> arguments) {
-//   sapa_penonton();
-//   double p, l, luas;
+String say2(String from, String message, [String to, String appName]){
+  return from + " say " + message + ((to != null) ? " to " + to : "") + ((appName != null) ? " via " + appName : "");
+}
 
-//   p = double.tryParse(stdin.readLineSync());
-//   l = double.tryParse(stdin.readLineSync());
+double luas_segiempat(double panjang, double lebar) => panjang * lebar;
 
-//   // luas = luas_segiempat(p, l);
+int doMathOperator(int number1, int number2, Function(int, int) operator){
+  return operator(number1, number2);
+}
 
-//   print(luas_segiempat(p, l));
-// }
+main(List<String> arguments) {
+  Function f;
+  f = luas_segiempat;
+  print(say("Johny", "Hello", appName: "Whatsapp"));
+  print(say2("Johny", "Hello", "Doloris"));
+  print(doMathOperator(1, 2, (a, b) => a * b));
+}
