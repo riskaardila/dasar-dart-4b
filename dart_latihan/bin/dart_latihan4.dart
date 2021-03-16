b35540254085cdd6fa9c464209ca11b1cec72655
riskaardila <riskaardila35@gmail.com>
2021-03-16 14:00:18 +0700

    tambahkan

--- dart_latihan/bin/dart_latihan4.dart
+++ dart_latihan/bin/dart_latihan4.dart
@@ -1,19 +1,19 @@
-// function
+// // function
 
-import 'dart:io';
+// import 'dart:io';
 
-String say(String from, String message, {String to, String appName}){
-  return from + " say " + message + ((to ))
-}
+// String say(String from, String message, {String to, String appName}){
+//   return from + " say " + message + ((to ))
+// }
 
-main(List<String> arguments) {
-  sapa_penonton();
-  double p, l, luas;
+// main(List<String> arguments) {
+//   sapa_penonton();
+//   double p, l, luas;
 
-  p = double.tryParse(stdin.readLineSync());
-  l = double.tryParse(stdin.readLineSync());
+//   p = double.tryParse(stdin.readLineSync());
+//   l = double.tryParse(stdin.readLineSync());
 
-  // luas = luas_segiempat(p, l);
+//   // luas = luas_segiempat(p, l);
 
-  print(luas_segiempat(p, l));
-}
+//   print(luas_segiempat(p, l));
+// }
